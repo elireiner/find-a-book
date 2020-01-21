8b055af9953ea645e042ab20d82fd6620549815b
Eli Reiners <goelyukim@gmail.com>
2020-01-21 13:34:25 -0500

    Add config fils and more

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import config from './config';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  state = {
+    books: [],
+    error: null
+  }
+  setBooks = books => {
+    this.setState({
+      books: books
+    })
+  }
+  constructUrl = (term) => {
+    const baseUrl = config.API_ENDPOINT + term + '&key=' + config.API_KEY;
+    console.log(baseUrl)
+    return baseUrl
+
+  }
+  handleSearch = e => {
+    e.preventDefault()
+    const { term } = e.target
+    fetch(this.constructUrl(term.value), {
+      method: 'GET'
+    })
+      .then(res => {
+        if (!res.ok) {
+          throw new Error(res.status)
+        }
+        return res.json()
+      })
+      .then(resJson => this.setBooks(resJson.items))
+      .catch(error => console.log(error))
+
+  }
+  render() {
+    const bookTitles = this.state.books.map(book => <p>{book.volumeInfo.title}</p>)
+    return (
+      <main className='App'>
+        <form
+          onSubmit={this.handleSearch}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <input
+            name='term' />
+          <button
+            type='submit'
+          >Search</button>
+        </form>
+        <div>
+          {bookTitles}
+        </div>
+      </main>
+    );
+  }
 }
 
 export default App;
---
+++ src/config.js
@@ -0,0 +1,5 @@
+export default {
+    API_ENDPOINT: `https://www.googleapis.com/books/v1/volumes?q=`,
+    API_KEY: 'AIzaSyCFd6vXFGyx-XxL4dfi0zz6PnANUDq1Fig',
+    //API_KEY: process.env.REACT_APP_API_KEY,
+  }
