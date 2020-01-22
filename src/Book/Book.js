35b85d2022b3d3018f4b940f964b659399c6d768
Eli Reiners <goelyukim@gmail.com>
2020-01-22 17:07:16 -0500

    Comlete auther rendering

--- src/Book/Book.js
+++ src/Book/Book.js
@@ -7,7 +7,7 @@ export default class Book extends Component {
             <li>
                 <h1>{this.props.volumeInfo.title}</h1>
                 <p>{this.props.volumeInfo.subtitle}</p>
-                {//Array.isArray(this.props.volumeInfo.authors) && console.log('hi')
+                {Array.isArray(this.props.volumeInfo.authors) &&
                 this.props.volumeInfo.authors.map(author => <p>{author}</p>)
                 }
            </li>
