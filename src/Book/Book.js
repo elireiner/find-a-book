65961dccc5d7b7545ff464f4a86ffe807ec8d8fd
Eli Reiners <goelyukim@gmail.com>
2020-01-22 16:55:56 -0500

    Initial code for rendering authors

--- src/Book/Book.js
+++ src/Book/Book.js
@@ -7,8 +7,8 @@ export default class Book extends Component {
             <li>
                 <h1>{this.props.volumeInfo.title}</h1>
                 <p>{this.props.volumeInfo.subtitle}</p>
-                {//Array.isArray(this.props.valumeInfo.authors) && console.log('hi')
-                console.log(this.props.valumeInfo)
+                {//Array.isArray(this.props.volumeInfo.authors) && console.log('hi')
+                this.props.volumeInfo.authors.map(author => <p>{author}</p>)
                 }
            </li>
         )
