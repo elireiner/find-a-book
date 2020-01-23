422c4df364d0373a120bee185fe242b0f327eb47
Eli Reiners <goelyukim@gmail.com>
2020-01-23 11:55:47 -0500

    Add filter for full text

--- src/App.js
+++ src/App.js
@@ -3,18 +3,32 @@ import config from './config';
 import BookList from './BookList/BookList'
 
 class App extends Component {
-  state = {
-    books: [],
-    error: null
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: [],
+      error: null,
+      Filter: null
+    };
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleOptions = this.handleOptions.bind(this);
   }
+
   setBooks = books => {
     this.setState({
       books: books
     })
   }
+  setFilter = type => {
+    this.setState({
+      Filter: type
+    })
+  }
   constructUrl = (term) => {
-    const baseUrl = config.API_ENDPOINT + term + '&key=' + config.API_KEY;
-    console.log(baseUrl)
+    let baseUrl = config.API_ENDPOINT + term + '&key=' + config.API_KEY;
+    console.log(baseUrl);
+    this.state.Filter === 'full' && baseUrl += '&Filter=full';
     return baseUrl
 
   }
@@ -34,17 +48,26 @@ class App extends Component {
       .catch(error => console.log(error))
 
   }
+  handleOptions(e) {
+    const { value } = e.target
+    this.setFilter(value)
+    console.log(value)
+  }
   render() {
     return (
       <main className='App'>
-        <form
-          onSubmit={this.handleSearch}
-        >
-          <input
-            name='term' />
-          <button
-            type='submit'
-          >Search</button>
+        <form onSubmit={this.handleSearch}>
+          <label htmlFor='term'>Search with a term</label>
+          <input name='term' id='term' />
+
+          <label htmlFor='full'>Filter for full text</label>
+          <select onChange={this.handleOptions}>
+            <option value='all'>any</option>
+            <option value='full'>full</option>
+          </select>
+          <button type='submit'>
+            Search
+          </button>
         </form>
         <div>
           <BookList books={this.state.books} />
--- src/Book/Book.js
+++ src/Book/Book.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 
 export default class Book extends Component {
-    
+
     render() {
         return (
             <li>
                 <h1>{this.props.volumeInfo.title}</h1>
                 <p>{this.props.volumeInfo.subtitle}</p>
-                {Array.isArray(this.props.volumeInfo.authors) &&
-                this.props.volumeInfo.authors.map(author => <p>{author}</p>)
-                }
-           </li>
+                <ul>
+                    {Array.isArray(this.props.volumeInfo.authors) &&
+                        this.props.volumeInfo.authors.map(author => <li>{author}</li>)}
+                </ul>
+            </li>
         )
     }
 }
